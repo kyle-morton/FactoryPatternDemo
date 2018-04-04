2c1de1514faf49ecb7ccbc8f578856875ee7f4c0
Morton <MORTONM@dfj.usa>
2018-04-04 13:20:06 -0500

    updated prompt

--- Factory.Simple/Program.cs
+++ Factory.Simple/Program.cs
@@ -13,26 +13,36 @@ namespace Factory.Simple
         {
             while(true)
             {
-                Console.WriteLine();
-                Console.WriteLine("Enter a cell phone type (a - android, i - iphone, b - blackberry, anything else - quit):" );
-                var key = Console.ReadKey().KeyChar;
-
+                var key = PromptForKey();
                 var phoneType = GetPhoneType(key);
-                if (phoneType != null) {
 
-                    var phone = new PhoneFactory().GetPhone(phoneType.Value);
-                    phone.CallSomeone();
-
-                }
-                else
+                if (phoneType == null)
                     break;
 
+                var phone = new PhoneFactory().GetPhone(phoneType.Value);
+                phone.CallSomeone();
+
                 Console.ReadLine();
+                Console.Clear();
             }
 
-            Console.WriteLine("Terminating...");
-            Console.ReadLine();
+            Terminate();
         }
+
+
+        private static char PromptForKey()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Enter a cell phone type:");
+            Console.WriteLine("a - android, i - iphone, b - blackberry, anything else - quit");
+            Console.WriteLine("-----------------------------------------");
+
+            var key = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            return key;
+        }
+
 
         private static PhoneType? GetPhoneType(char character)
         {
@@ -54,6 +64,13 @@ namespace Factory.Simple
             }
 
             return type;
+        }
+
+        private static void Terminate()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Terminating...");
+            Console.ReadLine();
         }
 
     }
